a12fc943c75738761711e2e45f628d92fb439112
Gala <galagalunia@gmail.com>
2018-12-21 22:00:53 -0500

    Add clickable project image. Remove project link from the project card footer

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,22 +4,25 @@ import "../css/projectCard.css";
 const ProjectCard = props => (
   <div className="projectCard">
     <div className="grid">
-      <div className="bckgr" style={{ backgroundColor: props.color }} />
-        <img
-          className="projectImage"
-          src={props.image}
-          alt="Thumbnail of the project"
-        />
-        <div className="textGrid">
-          <div className="footerColor" style={{ backgroundColor: props.color }}>
-            <div className="projectTitle">{props.name}</div>
-              <div className="links">
-  	           <a className="projectUrl" href={props.url}>
-  	           View the project
-  	           </a>
-	             <a className="projectSourceCode" href={props.sourceLink}>
-	              Source code
-	             </a>
+      <img
+        className="projectImage"
+        src={props.image}
+        alt="Thumbnail of the project"
+      />
+      <div className="textGrid">
+        <a
+          className="bckgr"
+          style={{ backgroundColor: props.color }}
+          href={props.url}
+        >
+        <div className="bckgr"></div>
+        </a>
+        <div className="footerColor" style={{ backgroundColor: props.color }}>
+          <div className="projectTitle">{props.name}</div>
+          <div className="links">
+            <a className="projectSourceCode" href={props.sourceLink}>
+              Source code
+            </a>
           </div>
         </div>
       </div>
