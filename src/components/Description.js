081b9d392ec0c6309420a55300625b6e3c6c9a87
Gala <galagalunia@gmail.com>
2020-06-16 21:29:50 +0200

    Change welcome text

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,7 @@ import "../css/description.css"
 
 const Description = (props) => (
 	<div className="description">
- 	  <p>I'm a QA Enginner & aspiring Front-End Developer.</p>
+ 	  <p>I'm a QA Enginner and Scrum Master based in Switzerland</p>
  	  <p>You can find me on <a className="socialMedia" href="https://www.linkedin.com/in/galasi">Linkedin</a> and <a className="socialMedia" href="https://github.com/GalkaS">Github</a>.</p>
 	</div>
 )
